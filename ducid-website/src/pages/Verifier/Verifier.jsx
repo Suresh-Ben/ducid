6d5ab430f9d75919877c94d2900097ed58abbfb0
Suresh-Ben <sureshbennabatthula@gmail.com>
2023-02-25 18:49:31 +0530

    VErifier frontend update

--- ducid-website/src/pages/Verifier/Verifier.jsx
+++ ducid-website/src/pages/Verifier/Verifier.jsx
@@ -2,10 +2,29 @@ import React from 'react';
 
 import './Verifier.css';
 
+import Navbar from '../../componenets/requires/Navbar';
+import Footer from '../../componenets/requires/Footer';
+
+import SimpleButton from '../../componenets/requires/Buttons/SimpleButton';
+import DataField from '../../componenets/requires/DataField';
+
 function Verifier() {
     return (
         <div>
-            <h1>This is Verifier Page</h1>
+            <div style={{minHeight:'100vh'}}>
+                <div className='nav-back'> <Navbar/> </div>
+
+                <div style={{margin:'1.5rem', display : 'flex', justifyContent: 'center'}}><div>
+                    <p>Please enter student ducid:</p>
+                    <input style={{width:'30rem'}} className='student-data' type="text" /> <SimpleButton text="Check student data"/>
+
+                    <div style={{margin : '1rem'}}>
+                        <DataField  />
+                        <DataField  />
+                    </div>
+                </div></div>
+            </div>
+            <Footer/>
         </div>
     );
 }
