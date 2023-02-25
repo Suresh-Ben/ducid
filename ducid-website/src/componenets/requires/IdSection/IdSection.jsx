002361663bf3a222852c06ea3f18953e04c74c55
Rahul_26 <91335522+Sainikhilnaidu9@users.noreply.github.com>
2023-02-25 16:30:04 +0530

    id component minor update

--- ducid-website/src/componenets/requires/IdSection/IdSection.jsx
+++ ducid-website/src/componenets/requires/IdSection/IdSection.jsx
@@ -8,7 +8,7 @@ function IdSection(props) {
         <div>
             <label className="lable" for="formGroupExampleInput">Your {props.purpose} id:</label><br />
             <div className="clgid" style={{display : "flex", height: '2rem'}}>
-                <input value={props.ducid}  disabled={true} style={{display : 'inline-block', width: '100%'}} type="text" />
+                <input value={props.ducid}  disabled={true} style={{paddingLeft:'.75rem', display : 'inline-block', width: '100%'}} type="text" />
                 <a style={{height : '2rem', display : 'inline-block', border: '1px', borderStyle: 'solid', borderRadius: '4px'}} >
                     <a style={{height: '2rem', padding: '0 8px'}} type="button" className="copybutton" onClick={() =>  navigator.clipboard.writeText(props.ducid)}><img style={{display : 'inline-block' , height : '1.5rem'}} src={Clipboard} alt = "This is Clipboard" /></a>
                 </a>
